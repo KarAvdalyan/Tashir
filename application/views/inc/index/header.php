7e1b3859868ca129ad173dc8aedde83a375366c6
aram25 <aram25@users.noreply.github.com>
2018-01-10 00:32:25 +0400

    dd

--- application/views/inc/index/header.php
+++ application/views/inc/index/header.php
@@ -78,16 +78,16 @@ Move the mouse over the button to open the dropdown menu.
   <div class="dropdown-content">
 
                                 <!-- Trigger the Payment with a button -->
-    <a class="btn btn-primary btn-md" id="insert" data-toggle="modal" data-target="#payment" href="#">Գործարք</a>
+    <a class="btn btn-info btn-md" id="insert" data-toggle="modal" data-target="#payment" href="#">Գործարք</a>
 
                                 <!-- Trigger the Product full with a button -->
-     <a class="btn btn-primary btn-md" data-toggle="modal" data-target="#add_product" href="#">Պրոդուկտի մուտքագրում</a>
+     <a class="btn btn-info btn-md" data-toggle="modal" data-target="#add_product" href="#">Պրոդուկտի մուտքագրում</a>
 
                                 <!-- Trigger the project full with a button -->
-     <a class="btn btn-primary btn-md" data-toggle="modal" data-target="#add_project" href="#">Պրոյեկտի մուտքագրում</a>
+     <a class="btn btn-info btn-md" data-toggle="modal" data-target="#add_project" href="#">Պրոյեկտի մուտքագրում</a>
 
                                 <!-- Trigger the ad_supplier full with a button -->
-     <a class="btn btn-primary btn-md" data-toggle="modal" data-target="#add_supplier" href="#">Մատակարարի մուտքագրում</a>
+     <a class="btn btn-info btn-md" data-toggle="modal" data-target="#add_supplier" href="#">Մատակարարի մուտքագրում</a>
   </div>
 </div>
 
@@ -99,13 +99,13 @@ Move the mouse over the button to open the dropdown menu.
     <div class="dropdown-content">
 
                                   <!-- Trigger the show Product  button -->
-              <a id="show_product"  class="btn btn-primary btn-md" href="#">Պրոդուկտի Դիտում</a>
+              <a id="show_product"  class="btn btn-info btn-md" href="#">Պրոդուկտի Դիտում</a>
 
                                   <!-- Trigger the show project full with a button -->
-              <a id="show_project" class="btn btn-primary btn-md" href="#">Պրոյեկտի Դիտում</a>
+              <a id="show_project" class="btn btn-info btn-md" href="#">Պրոյեկտի Դիտում</a>
 
                                   <!-- Trigger the show  show supplier full with a button -->
-              <a id="show_supplier"  class="btn btn-primary btn-md" href="#">Մատակարարի Դիտում</a>
+              <a id="show_supplier"  class="btn btn-info btn-md" href="#">Մատակարարի Դիտում</a>
                       
     </div>
 </div>
